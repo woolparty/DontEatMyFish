2a3a81f868f1024cbef1303edd98c349709b660e
unknown <rivershen@rivershen-PC1.tencent.com>
2015-08-11 00:22:57 +0800

    first commit

---
+++ DonTouchMyFish/Assets/Scripts/DragableItem.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragableItem : MonoBehaviour {
+
+	private Vector3 dist;
+	private float posX;
+	private float posY;
+
+	private bool isDragging;
+
+	void Start()
+	{
+		isDragging = false;
+	}
+
+
+	void Update()
+	{
+		if(isDragging)
+		{
+			rigidbody.angularVelocity = Vector3.zero;
+			//transform.localRotation = Quaternion.identity;
+		}
+
+		transform.localRotation = Quaternion.Euler(0,0,transform.rotation.eulerAngles.z);
+	}
+
+
+	void OnMouseDown()
+	{
+		isDragging = true;
+
+		dist = Camera.main.WorldToScreenPoint(transform.position);
+		posX = Input.mousePosition.x - dist.x;
+		posY = Input.mousePosition.y - dist.y;
+
+	}
+
+	void OnMouseDrag()
+	{
+		Vector3 curPos = new Vector3(Input.mousePosition.x - posX, dist.y, dist.z);
+		Vector3 worldPos = Camera.main.ScreenToWorldPoint(curPos);
+		transform.position = worldPos;
+	}
+
+	void OnMouseUp()
+	{
+		isDragging = false;
+
+	}
+}
