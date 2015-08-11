9a7fe3b3c3c3730ecc284d0381e6e49f353ee987
csy200926 <392140045@qq.com>
2015-08-11 21:37:08 +0800

    Simple code frame
    Simple code frame

---
+++ DonTouchMyFish/Assets/Assets/Scripts/GameControllers/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    #region Singleton
+    private static GameManager instance;
+    public static GameManager GetInstance()
+    {
+        if (instance == null)
+            Debug.LogError("No GameManager!!!");
+        return instance;
+    }
+    #endregion
+
+    public LevelController m_LevelController;
+
+
+    // Use this for initialization
+	void Awake ()
+	{
+	    instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
