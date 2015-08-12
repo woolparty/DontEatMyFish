6d341632cb965b86d6101665e9392187938e1a7a
csy200926 <392140045@qq.com>
2015-08-12 18:30:00 +0800

    C**
    C**

--- DonTouchMyFish/Assets/Assets/Scripts/DragableItem.cs
+++ DonTouchMyFish/Assets/Assets/Scripts/DragableItem.cs
@@ -22,7 +22,10 @@ public class DragableItem : MonoBehaviour {
 			//rigidbody.angularVelocity = Vector3.zero;
 			//transform.localRotation = Quaternion.identity;
 		}
+        
 
+	    Vector3 position = transform.localPosition;
+        transform.localPosition = new Vector3(position.x, position.y,0);
 		transform.localRotation = Quaternion.Euler(0,0,transform.rotation.eulerAngles.z);
 	}
 
@@ -41,7 +44,9 @@ public class DragableItem : MonoBehaviour {
 	{
 		Vector3 curPos = new Vector3(Input.mousePosition.x - posX, dist.y, dist.z);
 		Vector3 worldPos = Camera.main.ScreenToWorldPoint(curPos);
-		transform.position = worldPos;
+
+	    float diff = (worldPos - transform.position).x;
+        rigidbody.velocity = new Vector3(diff / Time.deltaTime, rigidbody.velocity.y, 0);
 	}
 
 	void OnMouseUp()
